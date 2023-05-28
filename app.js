79090151d8b28fd1fbb5e7c80e755f695253b532
Shaun <shaun@thenetninja.co.uk>
2023-05-28 08:45:28 +0100

    lesson-3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const readline = require('readline')
-const { generateMeta } = require('./controllers/openaiController')
+const { generateMeta, generateImage } = require('./controllers/openaiController')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-rl.question("YouTube Video Title: \n", (title) => generateMeta(title))
+// rl.question("YouTube Video Title: \n", (title) => generateMeta(title))
+rl.question("Thumbnail description: \n", (desc) => generateImage(desc))
--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -25,7 +25,17 @@ const generateMeta = async (title) => {
   })
 
   console.log(tags.data.choices[0].message)
-
 }
 
-module.exports = { generateMeta }
+const generateImage = async (desc) => {
+
+  const image = await openai.createImage({
+    prompt: desc,
+    n: 1,
+    size: '1024x1024'
+  })
+
+  console.log(image.data.data[0].url)
+}
+
+module.exports = { generateMeta, generateImage }
